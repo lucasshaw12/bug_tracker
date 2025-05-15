55b5cc91333050829f1a998de2f2c7e26d672c57
Lucasshaw12 <shaw51@hotmail.co.uk>
2025-05-15 19:43:01 +0100

    create and edit bug

--- bug_tracker/urls.py
+++ bug_tracker/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
-from .views import BugListView
+from .views import BugListView, BugUpdateView, BugCreateView
 
-urlpatterns = [path("", BugListView.as_view(), name="dashboard")]
+urlpatterns = [
+    path("", BugListView.as_view(), name="dashboard"),
+    path("bug/add/", BugCreateView.as_view(), name="bug_add"),
+    path("bug/<int:pk>/edit/", BugUpdateView.as_view(), name="bug_edit"),
+]
--- bug_tracker/views.py
+++ bug_tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, CreateView
 
 from .models import Bug
 
@@ -7,3 +8,27 @@ from .models import Bug
 class BugListView(ListView):
     model = Bug
     template_name = "dashboard.html"
+
+
+class BugCreateView(CreateView):
+    model = Bug
+    fields = [
+        "bug_title",
+        "bug_description",
+        "application_name",
+        "expected_behaviour",
+        "actual_behaviour",
+        "user_assigned_to",
+        "completion_status",
+        "complexity_level",
+        "severity_level",
+    ]
+    template_name = "bugs/bug_form.html"
+    success_url = reverse_lazy("dashboard")
+
+
+class BugUpdateView(UpdateView):
+    model = Bug
+    fields = BugCreateView.fields
+    template_name = "bugs/bug_form.html"
+    success_url = reverse_lazy("dashboard")
